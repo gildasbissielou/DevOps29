44273039cb761c18d258996b92d48187bdb6c7ac
Cloud User <ec2-user@ip-172-31-87-109.ec2.internal>
2022-08-20 01:39:34 +0000

    second commit

--- Test.sh
+++ Test.sh
@@ -1,1 +1,16 @@
-life is great
+#!/bin/bash
+#life is great please take note!
+echo "welcome to Truist Banking please enter your name"
+read "name"
+echo "name please enter your password"
+read "password"
+echo "would you like to check your balance"
+read "yes"
+echo "your balance is $ 189000.00"
+echo "enter the amount you would you like to withdraw in cash"
+read "amount"
+echo "your cash is below the dispencer"
+echo "Thanks for visiting Truist"
+date
+#we will add on as we move forward.
+
