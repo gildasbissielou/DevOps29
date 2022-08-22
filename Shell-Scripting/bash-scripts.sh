4f134f5bd1a180895574924bea5daa16e83ecf02
Cloud User <ec2-user@ip-172-31-87-109.ec2.internal>
2022-08-22 02:54:20 +0000

    mythirdcommit

---
+++ Shell-Scripting/bash-scripts.sh
@@ -0,0 +1,13 @@
+Introduction
+#what is Shell
+#Types of Shells
+#What is Shell Scirpting?
+#First shell script Program
+
+#!/bash/sh
+echo "hello Gildas please  enter your password"
+read "passwd"
+echo "welcome to DevOps-Engineerclass29A"
+date
+
+
