0a442747ce16c853a64dfb32a9e1fd0a4b134b79
Cloud User <ec2-user@ip-172-31-87-109.ec2.internal>
2022-08-24 22:35:31 +0000

    update

---
+++ Git-Github/Git-Summary/summary.sh
@@ -0,0 +1,95 @@
+
+Git Summary:
+===========
+ Git is a distributed version control system.
+   We use git for Versioning in our projects.
+ Git keeps/records changes made in codes/files/scripts
+
+ Basic concepts:
+  1. git installation
+       yum install git  [for RedHAT/centos]
+       apt install git  [for ubuntu and debain ]
+
+       download the microsoft installer file for git
+          Git-2.37.2.2-64-bit.exe
+          git bash
+
+  2. git commands
+      - git [init/add/commit -m/status/  ]
+      - git pull/fetch/clone/
+
+  3. git branch  = a branch is a line of development
+     - git branches:
+          dev stage master
+     - git branch branchName
+     - git branch -d branchName
+       git switch branchName or git checkout branchName
+       git checkout -b branchName
+
+  4. git tag
+  5. git merge
+  6. pull requests = PR
+Paul/
+Mary
+simon:
+   https://github.com/landmarktech29/visa
+Dominion:
+  git fetch/pull/clone
+  git clone https://github.com/landmarktech29/visa
+  git config --global user.name "Dominion"
+  git config --global user.email "Dominion@gmail.com"
+  git config --global --list
+
+1. Coding is done by Developers using programming LANDGUAGES
+   java, python, c++, JS, NodeJS, Groovy
+
+2. DevOps Engineers work with Developers
+
+2. DevOps Engineers write and modify scripts.
+   scripting LANDGUAGES:
+       bash shell scripts /
+       python scripts
+       groovy scripts(Jenkinsfiles)
+       yaml (Kubernetes and ansible)
+       terraform codes
+   The use SCM [GitHub, GitLab, etc.] to record changes in scripts
+
+ https://github.com/LandmakTechnology/scripting
+
+ACHIEVING ZERO DOWNTIME AND SUCCEEDING:
+   1.  Attend all live your classes
+        and follow videos shared on time. 3PM
+   2A. Revise videos and notes before the next class
+   2.  Consistently dedicated 3+ hours for daily studies
+   3.  Raise ticket of issues and share in the help desk and Solution groups.
+   4.  Be quick to help others reslove issues/tickets -
+
+
+
+
+=====
+90$/HR    = 90*8 * 5 * 52 = 187,200
+120$/HR   = 120*8*5*52    = 249,600
+170 + 10% = 170*1.1       = 187,000
+165 + 10% = 165*1.1       = 181.500
+  9AM -  5PM EST
+  12PM - 8PM EST
+795 + 811
+811,000USD
+
+======
+Coveth the blessings:
+12
+Now the Lord had said unto Abram, Get thee out of thy country, and from thy
+kindred, and from thy father's house, unto a land that I will shew thee
+
+And I will make of thee a great nation,and I will bless thee, and make thy
+name great; and thou shalt be a blessing
+
+And I will bless them that bless thee, and curse him that curseth thee
+and in thee shall all families of the earth be blessed.
+
+curses:
+  village /
+
+
